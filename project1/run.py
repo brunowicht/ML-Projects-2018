62f6635cd78ac718f4d25c2f1c4217cb205d0308
Bruno Wicht <bruno.wicht@epfl.ch>
2018-10-18 16:37:06 +0200

    some tries in run.py

--- project1/run.py
+++ project1/run.py
@@ -5,26 +5,39 @@ Created on Thu Oct 18 12:38:51 2018
 @author: bruno
 """
 
-from utils.cleaning import load_clean_standardize
+from utils.cleaning import *
 from utils.implementations import *
 from utils.proj1_helpers import *
 import numpy as np
 
 lambda_ = 0.8
-max_iter = 100
-gamma = 0.7
+max_iters = 50
+gamma = 0.0035
 
 
 # Load clean and standardize the data
-data_x, data_y, train_ids = load_clean_standardize("../../MLData2018/train.csv")
-test_x, test_y, test_ids = load_clean_standardize("../../MLData2018/test.csv")
+data_x, data_y, train_ids, means, stds = load_clean_standardize_train("../../MLData2018/train.csv")
 print("Cleaning done")
 
-initial_w = np.zeros(data_x.shape[1])
+gammas = np.linspace(0.01, 1, 101)
+best_gamma = 0.00
+best_loss = 1000
 
-w,loss = least_squares(data_y,data_x)
-print(w)
-print(loss)
+for g in gammas:
+    print(g)
+    initial_w = np.zeros(data_x.shape[1])
+    data_y_log_reg = (data_y/2) +0.5
+    w,loss = least_squares_GD(data_y, data_x,initial_w,max_iter,g)
 
-#y_pred = predict_labels(w, test_x)
+
+    loss = compute_loss(data_y, data_x, w)
+    print(loss)
+    if loss < best_loss:
+        best_loss = loss
+        best_gamma = g
+
+print(best_loss)
+print(best_gamma)
+#test_x, test_y, test_ids = load_clean_standardize_test("../../MLData2018/test.csv", means, stds)
+#y_pred = predict_labels(w, test_x, logistic=False)
 #create_csv_submission(test_ids, y_pred, "submission.csv")
