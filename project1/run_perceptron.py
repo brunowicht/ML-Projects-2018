b1cd31927f637ca666c732eb0433dfaab2151fee
NFreundl <nicolas.freundler@epfl.ch>
2018-10-24 16:33:37 +0200

    nice perceptron, still under .78

---
+++ project1/run_perceptron.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 12:57:33 2018
+
+@author: Nicolas
+"""
+
+from utils.cleaning import *
+from utils.implementations import *
+from utils.proj1_helpers import *
+import numpy as np
+import utils.perceptron as prc
+
+lambda_ = 0.8
+max_iters = 1000
+gamma = 0.3
+
+
+# Load clean and standardize the data
+#data_x, data_y, train_ids, means, stds = load_clean_standardize_train("C:/Users/Test/train.csv")
+print("Cleaning done")
+print(data_x.shape)
+#for i in np.logspace(-9,0,10):
+#    w=prc.train(data_y,data_x,np.array([0.]*496),250000,i)
+#    print("training done ")
+#    y_hat=prc.predictions(data_x[:250000],w)
+#    matches=np.heaviside(y_hat*data_y,.5)
+#    print(np.sum(matches)/250000)
+#    print(i)
+#1e-6 is the best one
+
+final_w=prc.bagging(20,data_y,data_x,np.array([.0]*496),250000,1e-6)
+y_hat=prc.predictions(data_x[:250000],final_w)
+
+#test_x, test_y, test_ids = load_clean_standardize_test("C:/Users/Test/test.csv", means, stds)
+#regressor=np.ones((test_x.shape[0],test_x.shape[1]+1))
+#regressor[:,1:]=test_x
+#y_pred = predict_labels(w, regressor, logistic=False)
+#create_csv_submission(test_ids, y_pred, "submission.csv")
---
+++ project1/utils/perceptron.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 11:18:46 2018
+
+@author: Nicolas
+"""
+import numpy as np
+import utils.cleaning as cln
+
+def train(y,tx,w,k,gamma):
+    x=-np.ones((496,))
+
+    for i in range(k):
+#        if i > 249000 and i< 250100:
+#            print(i%len(y))
+        idx=i%len(y)
+#        x=online_expansion_3d(tx[idx])
+        x[1:]=tx[i]
+        w=w+gamma*(y[idx]-np.sign(x.dot(w)))*x
+        w=w/np.sum(np.abs(w))
+    return w
+
+def online_expansion_3d(x):
+    d2x=[]
+    d3x=[]
+    for i in range(len(x)):
+        for j in range(i,len(x)):
+            d2x.append(x[i]*x[j])
+            
+    for i in range(len(x)):
+        for j in range(i,len(x)):
+            for k in range(j,len(x)):
+                d3x.append(x[i]*x[j]*x[k])
+    
+            
+    return np.array([-1.]+list(x)+d2x+d3x)
+
+def predictions(tx,w):
+    x=-np.ones((496,))
+    
+    y_pred=np.zeros((tx.shape[0],))
+    
+    for i in range(tx.shape[0]):
+#        x=online_expansion_3d(tx[i])
+        x[1:]=tx[i]
+        y_pred[i]=np.sign(w.dot(x))
+    return y_pred
+
+def bagging(nb_experiment,data_y,data_x,w,k,gamma):
+    init_w=w
+    res_w=np.zeros((data_x.shape[1]+1,))
+    for i in range(nb_experiment):
+        data_y,data_x=cln.shuffle(data_y,data_x)
+        res_w=res_w+train(data_y,data_x,init_w,k,gamma)
+    res_w=res_w/nb_experiment
+    return res_w
+    
+    
+
+        
