413c9c7f6f4de12fe4b7c7b78ddf860c7a86852a
NFreundl <nicolas.freundler@epfl.ch>
2018-10-24 17:49:39 +0200

    perceptron almost done

--- project1/run_perceptron.py
+++ project1/run_perceptron.py
@@ -11,13 +11,11 @@ from utils.proj1_helpers import *
 import numpy as np
 import utils.perceptron as prc
 
-lambda_ = 0.8
-max_iters = 1000
-gamma = 0.3
+
 
 
 # Load clean and standardize the data
-#data_x, data_y, train_ids, means, stds = load_clean_standardize_train("C:/Users/Test/train.csv")
+data_x, data_y, train_ids, means, stds = load_clean_standardize_train("C:/Users/Test/train.csv")
 print("Cleaning done")
 print(data_x.shape)
 #for i in np.logspace(-9,0,10):
@@ -29,11 +27,12 @@ print(data_x.shape)
 #    print(i)
 #1e-6 is the best one
 
-final_w=prc.bagging(20,data_y,data_x,np.array([.0]*496),250000,1e-6)
-y_hat=prc.predictions(data_x[:250000],final_w)
+final_w=prc.bagging(30,data_y,data_x,np.array([.0]*496),250000,1e-6)
+print(np.sum(np.heaviside(data_y*prc.predictions(data_x[:250000],final_w),.5))/len(data_y))
 
 #test_x, test_y, test_ids = load_clean_standardize_test("C:/Users/Test/test.csv", means, stds)
 #regressor=np.ones((test_x.shape[0],test_x.shape[1]+1))
 #regressor[:,1:]=test_x
-#y_pred = predict_labels(w, regressor, logistic=False)
-#create_csv_submission(test_ids, y_pred, "submission.csv")
+#y_pred=prc.predictions(test_x[:250000],w)
+
+#create_csv_submission(test_ids, y_pred, "hoc_erat_in_votis.csv")
