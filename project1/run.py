edab6025314771e6cf36336fa37786cdc54fbde7
Johan Lanzrein <johan.lanzrein@epfl.ch>
2018-10-29 18:57:45 +0100

    final touchs

--- project1/run.py
+++ project1/run.py
@@ -1,53 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 18 12:38:51 2018
+Created on Wed Oct 24 12:57:33 2018
 
-@author: bruno
+@author: Nicolas
 """
 
 from utils.cleaning import *
 from utils.implementations import *
 from utils.proj1_helpers import *
 import numpy as np
+import utils.perceptron as prc
+import matplotlib.pyplot as plt
 
-lambda_ = 0.8
-max_iters = 1000
-gamma = 0.3
+plotting=True
 
 
+PATH = "../../"
 # Load clean and standardize the data
-data_x, data_y, train_ids, means, stds = load_clean_standardize_train("train.csv")
-print("Cleaning done")
-print(data_x.shape)
-
-gammas = np.linspace(0.000001, 0.000004, 4)
-lambdas = np.linspace(0.0000,0.0000002,21)
-best_gamma = 0.00
-best_loss = 1000
-best_lambda = 0.00
-#put the first parameter: constant column 1
-regressor=np.ones((data_x.shape[0],data_x.shape[1]+1))
-regressor[:,1:]=data_x
-for l in lambdas:
-    print("lambda: "+str(l))
-    initial_w = np.zeros(regressor.shape[1])
-    data_y_log_reg = (data_y/2) +0.5
-    w,loss = ridge_regression(data_y, regressor, l)
-
-    loss = compute_loss(data_y,regressor, w)
-    print(loss)
-    if loss < best_loss:
-        best_loss = loss
-        best_lambda = l
-
-print("done")        
-print("best loss: "+str(best_loss))
-print("best lambda: "+str(best_lambda))
-w,loss = ridge_regression(data_y, regressor, best_lambda)
+train_file = PATH+"train.csv"
+test_file = PATH+"test.csv"
+#data_x, data_y, train_ids, means, stds = load_clean_standardize_train(train_file)
+#best_gamma = 1.0e-06
+filename = "ave_maria.csv"
+#
+print("Done loading data and cleaning it. \n Starting perceptron with gamma = %f" % best_gamma)
+#
+#
+#
+###1e-6 is the best one for sgd
+#
+final_w=prc.bagging(50,data_y,data_x,np.array([.0]*(data_x.shape[1]+1)),250000,best_gamma)
+print("Found features for the model. \n Now starting to compute selection of best features")
 
 
-test_x, test_y, test_ids = load_clean_standardize_test("../../MLData2018/test.csv", means, stds)
-regressor=np.ones((test_x.shape[0],test_x.shape[1]+1))
-regressor[:,1:]=test_x
-y_pred = predict_labels(w, regressor, logistic=False)
-create_csv_submission(test_ids, y_pred, "submission.csv")
+## Best selection for now : 450 
+
+## We now select the best features according to their weight. We found 450 to be a good zone. 
+selection = 450
+
+#list the features from the less important to the most important
+liste=np.argsort(np.abs(final_w))
+liste=liste[liste>0] #excluding the 0 than can occur
+liste=liste-1 #because we calculated with the x0=-1, but we remove it for the input data
+
+if plotting:
+
+    plt.plot(np.abs(final_w[liste]))
+    plt.xlabel("sorting of absolute weights")
+    plt.ylabel("absolute weights")
+    plt.title("")
+    plt.show()
+reduced_data=data_x[:,liste[selection:]]
+final_w_2=prc.bagging(100,data_y,reduced_data,np.array([.0]*(data_x.shape[1]+1-selection)),250000,best_gamma)
+print("Found most heavy features\n Now computing final accuracy on training set")
+print("Final accuracy of the perceptron on the train set : %f " % (np.sum(np.heaviside(data_y*prc.predictions(reduced_data,final_w_2),.5))/len(data_y)))
+
+print("Using model on test dataset.")
+test_x, test_y, test_ids = load_clean_standardize_test(test_file, means, stds)
+test_x_r =test_x[:,liste[selection:]]
+print("Predicting values with model")
+y_pred=prc.predictions(test_x_r,final_w_2)
+print("Creating submission..under "+filename)
+create_csv_submission(test_ids, y_pred, filename)
+
+print("Done.")
+
---
+++ project1/run_test.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 12:38:51 2018
+
+@author: bruno
+"""
+
+from utils.cleaning import *
+from utils.implementations import *
+from utils.proj1_helpers import *
+import numpy as np
+
+lambda_ = 0.8
+max_iters = 1000
+gamma = 0.3
+
+
+# Load clean and standardize the data
+data_x, data_y, train_ids, means, stds = load_clean_standardize_train("train.csv")
+print("Cleaning done")
+print(data_x.shape)
+
+gammas = np.linspace(0.000001, 0.000004, 4)
+lambdas = np.linspace(0.0000,0.0000002,21)
+best_gamma = 0.00
+best_loss = 1000
+best_lambda = 0.00
+#put the first parameter: constant column 1
+regressor=np.ones((data_x.shape[0],data_x.shape[1]+1))
+regressor[:,1:]=data_x
+for l in lambdas:
+    print("lambda: "+str(l))
+    initial_w = np.zeros(regressor.shape[1])
+    data_y_log_reg = (data_y/2) +0.5
+    w,loss = ridge_regression(data_y, regressor, l)
+
+    loss = compute_loss(data_y,regressor, w)
+    print(loss)
+    if loss < best_loss:
+        best_loss = loss
+        best_lambda = l
+
+print("done")        
+print("best loss: "+str(best_loss))
+print("best lambda: "+str(best_lambda))
+w,loss = ridge_regression(data_y, regressor, best_lambda)
+
+
+test_x, test_y, test_ids = load_clean_standardize_test("../../MLData2018/test.csv", means, stds)
+regressor=np.ones((test_x.shape[0],test_x.shape[1]+1))
+regressor[:,1:]=test_x
+y_pred = predict_labels(w, regressor, logistic=False)
+create_csv_submission(test_ids, y_pred, "submission.csv")
