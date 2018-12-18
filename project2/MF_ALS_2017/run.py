dbbc59140e65a1e048dee41015f62a7e7362a34b
Bruno Wicht <bruno.wicht@epfl.ch>
2018-12-18 13:36:45 +0100

    add last year code that make 1.019 score

---
+++ project2/MF_ALS_2017/run.py
@@ -0,0 +1,31 @@
+from helpers import *
+from helpers import *
+from ALS import *
+
+# Load the data
+path_dataset = "data/data_train.csv"
+
+print('Loading the data...')
+ratings = load_data(path_dataset)
+
+# Split in training and testing sets
+print('\nSplitting the data in train and test sets...')
+train, test = split_data(ratings, p_test=0.1)
+
+# Generate predictions for 6 different models
+X, X_train, y_train, X_test, y_test = get_ALS_predictions(ratings, train, test, n_features_array=range(1, 31),lambda_user=0.2,lambda_item=0.02)
+
+# Linear blend of the previous models computed on the test set.
+clf = get_linear_clf(X_test, y_test)
+print('\nRMSE Train: %f' % calculate_rmse(clf.predict(X_train.T), y_train))
+
+
+print('RMSE Test: %f' % calculate_rmse(clf.predict(X_test.T), y_test))
+
+# Final predicted labels matrix
+predicted_labels = clf.predict(X.T).round().astype(int).reshape(ratings.shape)
+predicted_labels[predicted_labels > 5] = 5
+predicted_labels[predicted_labels < 1] = 1
+
+# Generate the CSV submission file
+generate_submission(predicted_labels)
